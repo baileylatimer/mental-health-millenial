a081f14686b7e063088d6a53641864fbd0f2f823
Bailey Latimer <latimerdesignco@gmail.com>
2022-03-21 16:22:53 -0700

    created hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,12 +27,19 @@ return (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <section class="project-page padding-y-xl flex flex-column items-center">
-      <div class="hero" >
-        <Image filename="home-hero.jpg" style={{ height: "825px", width: "100%" }} imgStyle={{ objectFit: "cover" }} />
-        <div class="content">
-          <h1 class="text-xxxxl color-bg">Welcome to your mental freedom.</h1>
+      <main class="margin-sm">
+        <div class="hero radius-lg" >
+          {/* <Image filename="home-hero.jpg" style={{ height: "825px", width: "100%" }} imgStyle={{ objectFit: "cover" }} /> */}
+          <div class="content position-relative">
+            <h1 class="text-xxxxl color-bg">Welcome to your mental freedom.</h1>
+            <p class="text-lg color-bg margin-top-sm"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit ac amet et rutrum curabitur.</p>
+            <div class="flex">
+              <a class="btn btn--primary margin-right-sm">Read more</a>
+              <a class="btn btn--accent">Contact</a>
+            </div>
+          </div>
         </div>
-      </div>
+      </main>
     </section>
 
   </>
